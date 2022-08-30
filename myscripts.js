5ca6816b3b5eed5a359b48a448489185396a75c9
griffinc8 <griffincoccari@gmail.com>
2022-08-30 11:00:23 -0500

    Got the numbers into the display, not formatted

--- myscripts.js
+++ myscripts.js
@@ -1,2 +1,33 @@
-let button = document.querySelector(".display")
-console.log(button)
+
+
+
+
+
+
+
+
+
+
+/* declare variable before forEach function */
+let buttons = document.querySelectorAll(".button");
+let number = 0
+let placement = document.querySelector("#placement")
+
+buttons.forEach((button) => {
+    // and for each one we add a 'click' listener
+    button.addEventListener('click', () => {
+        number = Number(button.outerText)
+        const content = document.createElement('div')
+        content.innerHTML =  `${number}`
+        placement.appendChild(content)
+    });
+  });
+
+
+
+
+
+
+
+
+
