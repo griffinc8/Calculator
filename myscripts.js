a2810e795429a01ef7ed5d279b3369008685eaf4
griffinc8 <griffincoccari@gmail.com>
2022-08-31 23:45:32 -0500

    used if statments to distinguish between operators and numbers

--- myscripts.js
+++ myscripts.js
@@ -23,37 +23,51 @@ function divide(num1, num2) {
 
 /* declare variable before forEach function */
 let buttons = document.querySelectorAll(".button");
-let placement = document.querySelector("#placement")
-let storage = []
+let placement = document.querySelector("#placement");
+let operators = document.querySelectorAll(".button.operator")
+let storage = [];
+
+console.log(buttons.className)
 
 buttons.forEach((button) => {
     // and for each one we add a 'click' listener
     button.addEventListener('click', () => {
         
         /* Clear HTML Content In display(placement) each time you click a new button */
-        placement.innerHTML = ""
 
-        let digit = button.outerText
-        storage.push(digit)
+        if (button.className == "button") {
 
-        var storageLength = storage.length;
-        var base_st = storage[0]
+            placement.innerHTML = ""
 
-        for (var i = 1; i < storageLength; i++) {
-            base_st = base_st.concat(storage[i])  
+            let digit = button.outerText
+            storage.push(digit)
+    
+            var storageLength = storage.length;
+            var base_st = storage[0]
+    
+            for (var i = 1; i < storageLength; i++) {
+                base_st = base_st.concat(storage[i])  
+            }
+    
+            const content = document.createElement('div')
+            content.innerHTML =  `${Number(base_st)}`
+            placement.appendChild(content)
+            
+        } else if (button.className == "button operator") {
+            operators.forEach((operator) => {
+                operator.addEventListener("click", () => {
+                    placement.innerHTML = ""
+                })
+            })            
         }
 
-        const content = document.createElement('div')
-        content.innerHTML =  `${Number(base_st)}`
-        placement.appendChild(content)
-        /* console.log(storage) */
+
+
+
     });
-    storage = storage
   });
 
 
 
-let operators = document.querySelectorAll(".button-operator")
-console.log(operators)
-
-
+/* I think we have to have all buttons share a class.
+ But need to have three classes to distinguish whether the button is an operator or a number */
