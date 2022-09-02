b722c7a57bee51ec988018cc6d6bc08723a6ea4c
griffinc8 <griffincoccari@gmail.com>
2022-09-02 18:04:44 -0500

    looking to implement conditionals

--- myscripts.js
+++ myscripts.js
@@ -25,9 +25,9 @@ function divide(num1, num2) {
 let buttons = document.querySelectorAll(".button");
 let placement = document.querySelector("#placement");
 let operators = document.querySelectorAll(".button.operator")
+let chosen_operator = []
 let storage = [];
-
-console.log(buttons.className)
+let storage1 = []
 
 buttons.forEach((button) => {
     // and for each one we add a 'click' listener
@@ -35,39 +35,47 @@ buttons.forEach((button) => {
         
         /* Clear HTML Content In display(placement) each time you click a new button */
 
-        if (button.className == "button") {
+        if (chosen_operator.length == 0) {
+            if (button.className == "button") {
 
-            placement.innerHTML = ""
+                placement.innerHTML = ""
 
-            let digit = button.outerText
-            storage.push(digit)
+                /* this puts the first element in the storage list */
     
-            var storageLength = storage.length;
-            var base_st = storage[0]
+                let digit = button.outerText
+                storage.push(digit)
+
+                /* used both base_st and storage to concatenate them together although they are the same.
+                 I am essentially concaetenating it with itself*/
+        
+                var storageLength = storage.length;
+                var base_st = storage[0]
+                
     
-            for (var i = 1; i < storageLength; i++) {
-                base_st = base_st.concat(storage[i])  
+                /* takes base_st and concatenates it */
+                for (var i = 1; i < storageLength; i++) {
+                    base_st = base_st.concat(storage[i])  
+                }
+                let combined_number = Number(base_st)
+        
+                const content = document.createElement('div')
+                content.innerHTML =  `${combined_number}`
+                placement.appendChild(content)
+                
+            } else if (button.className == "button operator") {
+                operators.forEach((operator) => {
+                    operator.addEventListener("click", () => {
+                        placement.innerHTML = ""
+                        chosen_operator.push(operator.outerText)
+                    })
+                })            
             }
     
-            const content = document.createElement('div')
-            content.innerHTML =  `${Number(base_st)}`
-            placement.appendChild(content)
-            
-        } else if (button.className == "button operator") {
-            operators.forEach((operator) => {
-                operator.addEventListener("click", () => {
-                    placement.innerHTML = ""
-                })
-            })            
+        console.log(chosen_operator)
         }
-
-
-
-
+        
     });
-  });
+});
 
 
-
-/* I think we have to have all buttons share a class.
- But need to have three classes to distinguish whether the button is an operator or a number */
+/* I want chosen operator to only have one thing in it */
